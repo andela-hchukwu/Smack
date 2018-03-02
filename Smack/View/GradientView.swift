26eba3c2957326f41bd239b5ce71b351c67365dc
Henry Chukwu <henry.chukwu@andela.com>
2018-03-02 13:17:24 +0100

    SWReveal

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,13 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Henry Chukwu on 3/2/18.
+//  Copyright © 2018 Henry Chukwu. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+}
